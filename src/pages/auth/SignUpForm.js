b38df7a81530129f568d4014b7106f5150eedbcf
Laurie Crean <lmcrean@gmail.com>
2024-04-12 18:23:54 +0000

    Refactor SignUpForm component to include error messages

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -24,7 +24,12 @@ const SignUpForm = () => {
   });
   const { username, password1, password2 } = signUpData;
 
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({
+    username: [],
+    password1: [],
+    password2: [],
+    non_field_errors: [],
+  });
 
   const history = useHistory();
 
