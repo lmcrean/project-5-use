83ec655e0e9d66e98db5431292b644a2119d68c7
Laurie Crean <lmcrean@gmail.com>
2024-04-13 13:45:09 +0000

    Fix axios default base URL in axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://project5-lmcrean-1b4c7e21126b.herokuapp.com//";
+axios.defaults.baseURL = "https://project5-lmcrean-1b4c7e21126b.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
