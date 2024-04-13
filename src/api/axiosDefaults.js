02629b8ef8c49341639b03c7373876cb451ad8ef
Laurie Crean <lmcrean@gmail.com>
2024-04-13 13:24:30 +0000

    Refactor CSS and import stylesheets

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://project5-react-lmcrean-f1b3523e8fec.herokuapp.com//";
+axios.defaults.baseURL = "https://project5-react-lmcrean-f1b3523e8fec.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
