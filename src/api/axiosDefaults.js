7a83e27f2a41be434889e63f9df68528112861fd
Laurie Crean <lmcrean@gmail.com>
2024-04-14 11:28:15 +0000

    Update API endpoint in axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://project5-lmcrean-1b4c7e21126b. herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
