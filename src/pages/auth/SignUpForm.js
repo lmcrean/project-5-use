1f57c62683a6bc388deedfe7f024687260ee4904
Laurie Crean <lmcrean@gmail.com>
2024-04-14 11:09:59 +0000

    Fix API endpoint in SignUpForm.js

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -43,7 +43,7 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("dj-rest-auth/registration/", signUpData);
+      await axios.post("/dj-rest-auth/registration/", signUpData);
       history.push("/signin");
     } catch (err) {
       setErrors(err.response?.data);
