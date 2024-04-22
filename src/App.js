0c6709ec99b9369db18c982ad1c40b735c5a7b72
Laurie Crean <lmcrean@gmail.com>
2024-04-22 17:33:34 +0000

    add PostPage import to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 
+import PostPage from "./pages/posts/PostPage";
+
 function App() {
   return (
     <div className={styles.App}>
@@ -17,6 +19,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
